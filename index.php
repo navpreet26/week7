1db5946f37aa3af3a7088efca8b813718092f341
navpreet26 <145068133+navpreet26@users.noreply.github.com>
2024-06-15 18:08:33 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,8 @@
+<!-- /index.php -->
+<?php include 'includes/header.php'; ?>
+<main>
+<h1>THE FAMOUS SINGER</h1>
+<p>The "GOAT" of punjabi music industry Sidhu Moose Wala was renowned for his ability to combine classic Punjabi music with rap and hip-hop in a way that resonated with fans throughout the Punjabi diaspora.</p>
+<img src="images/SIDHU.jpeg" alt="SIDHU MOOSE WALA">
+</main>
+<?php include 'includes/footer.php'; ?>
